e110251dd7bb51e50591a1de1dd5b672bdda4336
johnidfet <johnidfet@gmail.com>
2015-06-03 21:49:55 +0800

    update mapper_s2

--- FinalProject_v1/WC2_Small_Test/mapper_s2.cpp
+++ FinalProject_v1/WC2_Small_Test/mapper_s2.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+
 int main(int argv, char* argc[])
 {
-    for ( int i = 0; i < argv; i++ )
-        std::cout << argc[i] << std::endl;
+    std::string objective_good(argc[1]);
+
+    std::string value_shop;
+    std::string value_good;
+    int key_number;
+    while( 
+               std::cin >> value_good 
+            && std::cin >> value_shop 
+            && std::cin >> key_number
+    )
+    {
+        if( objective_good == value_good )
+        {
+            std::cout << key_number << '\t' << value_shop << '\t' << value_good << std::endl;
+        }
+    }
+    
 }
