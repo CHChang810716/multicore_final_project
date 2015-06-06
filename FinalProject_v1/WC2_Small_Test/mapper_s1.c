b45bb8e79b250bde988fb9cda60d63934a7d43b5
CHChang810716 <CHChang810716@gmail.com>
2015-06-06 16:28:56 +0800

    merge s1 untested

--- FinalProject_v1/WC2_Small_Test/mapper_s1.c
+++ FinalProject_v1/WC2_Small_Test/mapper_s1.c
@@ -1,10 +1,42 @@
-#include <iostream>
+#include <stdio.h>
+#include <string.h>
 
-int main(int argv, char** argc, char** env)
-{
-	for(int i = 0; env[i] != NULL; i ++)
-	{
-		std::cout << env[i] <<std::endl;
+#define BUF_SIZE 1048576
+#define DELIMITERS "=,;.()\"'/ \t"
+
+int main(int argc, char ** argv, char ** env){
+
+	char line[BUF_SIZE];
+	int i;
+	char** temp;
+	char shop[6];
+	for (temp = env; *temp != 0; temp++){
+		char *envline = strtok(*temp, "=/");
+		if (strcmp(envline, "map_input_file")) continue;
+		while(envline != NULL){
+			if (!strncmp(envline, "shop", 4)){
+				strncpy(shop, envline, 5);
+				shop[5] = '\0';
+			}
+			envline = strtok(NULL, "=/");
+		}
 	}
+	
+	while(fgets(line, BUF_SIZE - 1, stdin) != NULL){
+		for(i = strlen(line); i >= 0; i--){
+			if(line[i] == '\n' || line[i] == '\r'){
+				line[i] = '\0';
+			}
+		}
+
+		char * token = strtok(line, DELIMITERS);
+		
+		while(token != NULL){			
+			printf("%s\t%s\t1\n", token, shop);
+			token = strtok(NULL, DELIMITERS);
+		}
+	}
+	
+	return 0;
 }
 
