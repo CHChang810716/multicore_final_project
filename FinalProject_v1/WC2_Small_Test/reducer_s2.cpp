a582ba8b4422236b21d9e91fdf50810a0c744e98
CHChang810716 <CHChang810716@gmail.com>
2015-06-12 04:13:52 +0800

    update all

--- FinalProject_v1/WC2_Small_Test/reducer_s2.cpp
+++ FinalProject_v1/WC2_Small_Test/reducer_s2.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 #include <fstream>
 #include <string>
 #include <map>
@@ -7,24 +8,25 @@ typedef std::pair<std::string, std::string> StrPair;
 int main(int argv, char** argc)
 {
     const int first_n_result(3);
-    std::map<int, StrPair> result;
+    std::map<int, std::vector<StrPair> > result;
     std::string key;
     std::string value1;
     std::string value2;
     while(std::cin >> key >> value1 >> value2)
     {
-        result[atoi(key.c_str())] = StrPair(value1, value2);
+        result[atoi(key.c_str())].push_back( StrPair(value1, value2));
     }
     int count (0);
-    for(std::map<int, StrPair>::reverse_iterator rit (result.rbegin()); rit != result.rend() && count < 3; ++rit, ++count)
+    for(std::map<int, std::vector<StrPair> >::reverse_iterator rit (result.rbegin()); rit != result.rend() && count < 3; ++rit)
     {
-        std::cout 
-            << rit->first 
-            << '\t'
-            << rit->second.first 
-            << '\t'
-            << rit->second.second 
-            << std::endl;
+	for(std::vector<StrPair>::iterator vit(rit->second.begin()); vit != rit->second.end() && count < 3; ++vit, ++count)
+		std::cout 
+			<< rit->first 
+			<< '\t'
+			<< vit->first 
+			<< '\t'
+			<< vit->second 
+			<< std::endl;
         //count ++;
     }
 }
